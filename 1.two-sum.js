8d5f25c769c0b6c052fa14c21af2cf13c55c76c0
Devin Valencia <DevinValencia@yahoo.com>
2021-11-22 14:37:40 -0500

    Posible Solution

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -10,8 +10,28 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    var test = 0;
+var twoSum = function (nums, target) {
+    var indexOne, indexTwo = -1;
+
+    for (var n = 0; n < nums.length; n++) {
+        var balance = target - nums[n];
+        indexOne = nums.indexOf(nums[n]);
+
+        for (var m = n + 1; m < nums.length; m++) {
+            if (nums[m] == balance) {
+                indexTwo = nums.indexOf(nums[m]);
+            }
+        }
+
+        if (indexTwo != -1) {
+            n = nums.length;
+        }
+    }
+
+    var indicies = [];
+    indicies.push(indexOne);
+    indicies.push(indexTwo);
+
+    return indicies;
 };
 // @lc code=end
-
